6c849053644d1e6ea7fed0667f92d5a399b96ae5
plan222 <52143360+plan222@users.noreply.github.com>
2020-10-17 10:14:07 +0900

    react-day-picker
    MakeReservation 적용

--- screen/src/pages/MakeReservation.js
+++ screen/src/pages/MakeReservation.js
@@ -1,8 +1,35 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import PetCard from './PageComponents/Petcard';
+import DayPicker from 'react-day-picker';
+import 'react-day-picker/lib/style.css';
 
 const MakeReservation = () => {
+    const MONTHS = [
+        '1월',
+        '2월',
+        '3월',
+        '4월',
+        '5월',
+        '6월',
+        '7월',
+        '8월',
+        '9월',
+        '10월',
+        '11월',
+        '12월',
+      ];
+      const WEEKDAYS_LONG = [
+        '일요일',
+        '월요일',
+        '화요일',
+        '수요일',
+        '목요일',
+        '금요일',
+        '토요일',
+      ];
+      const WEEKDAYS_SHORT = ['일', '월', '화', '수', '목', '금', '토'];
+
     return (
         <div className="page">
             <div className="top_nav nav_with_title">
@@ -16,6 +43,11 @@ const MakeReservation = () => {
                 <div className="sub_menu bold_20 margin_top_16">나의 반려동물</div>
                 <PetCard />
                 <div className="sub_menu bold_24 margin_top_16">예약 일시</div>
+                <DayPicker 
+                    months={MONTHS}
+                    weekdaysLong={WEEKDAYS_LONG}
+                    weekdaysShort={WEEKDAYS_SHORT}
+                />
             </div>
         </div>
     );
