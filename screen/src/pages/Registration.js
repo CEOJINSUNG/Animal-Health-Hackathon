87438db109f2d907cfca2df756c2b6b569368821
plan222 <52143360+plan222@users.noreply.github.com>
2020-12-28 12:48:29 +0900

    Input Form Update
    등록페이지, AI진단 페이지 state 적용완료

--- screen/src/pages/Registration.js
+++ screen/src/pages/Registration.js
@@ -4,6 +4,33 @@ import { Link } from 'react-router-dom';
 
 
 const Registration = () => {
+    const [kind, setKind] = useState("");
+    const [name, setName] = useState("");
+    const [detail, setDetail] = useState("");
+    const [sex, setSex] = useState("");
+    const [month, setMonth] = useState(0);
+    async function Registration() {
+        signInWithGoogle();
+        const User = auth.currentUser;
+        setTimeout(() => {
+            firestore.collection("UserInfo").doc(User.uid)
+            .set({
+                AnimalKind : kind,
+                name: name,
+                DetailKind : detail,
+                sex: sex,
+                age: month
+            })
+        }, 1500)
+    }
+
+    const onClickDog = () => {setKind("강아지");}
+    const onClickCat = () => {setKind("고양이");}
+    const onChangeName= (e) =>{setName(e.target.value);}
+    const onChangeMonth =(e) =>{setMonth(e.target.value);}
+    const onChangeDetail =(e) =>{setDetail(e.target.value);}
+    const onClickMale = () =>{setSex("수컷");}
+    const onClickFemale = () =>{setSex("암컷");}
     return (
         <div className="page">
             <div className="top_nav nav_with_title">
@@ -18,30 +45,39 @@ const Registration = () => {
                     <div className="select_box_container">
                         <span>
                             <div className="select_box"><img className="img_center" src={require('./PageComponents/icon/dog.png')} alt="강아지" width="90px"/></div>
+                            <div 
+                                className={kind=="강아지" ? 'regi_button_on select_box': 'select_box'} 
+                                onClick={onClickDog}>
+                                <img className="img_center" src={require('./PageComponents/icon/dog.png')} alt="강아지" width="90px"/>
+                            </div>
                             <div className="select_text regular_20">강아지</div>
                         </span>
                         <span>
-                            <div className="select_box"><img className="img_center" src={require('./PageComponents/icon/cat.png')} alt="고양이" width="90px"/></div>
+                            <div 
+                                className={kind=="고양이" ? 'regi_button_on select_box': 'select_box'} 
+                                onClick={onClickCat}>
+                                <img className="img_center" src={require('./PageComponents/icon/cat.png')} alt="고양이" width="90px"/>
+                            </div>
                             <div className="select_text regular_20">고양이</div>
                         </span>
                     </div>
                 </div>
                 <form>
-                    <input className="reg_form" type="text" placeholder="이름을 입력하세요"/>
-                    <select className="reg_form">
+                    <input className="reg_form" type="text" placeholder="이름을 입력하세요" onChange={onChangeName}/>
+                    <select className="reg_form" onChange={onChangeDetail}>
                         <option value="" disabled selected>품종</option>
 	                    <option value="rab">래브라도 리트리버</option>
                         <option value="sit">시츄</option>
                         <option value="fom">포메라니안</option>
                     </select>
                     <div className="reg_form gender_box_container">
-                        <div className="gender_box radius_10">수컷</div>
-                        <div className="gender_box radius_10">암컷</div>
+                        <div className={sex=="수컷" ? 'gender_box_male gender_box': 'gender_box color_blue'} onClick={onClickMale}>수컷</div>
+                        <div className={sex=="암컷" ? 'gender_box_female gender_box': 'gender_box color_pink'} onClick={onClickFemale}>암컷</div>
                     </div>
-                    <input className="reg_form" type="text" placeholder="나이를 입력하세요"/>
-                    <div className="reg_submit background_orange color_white bold_20">
+                    <input className="reg_form" type="text" placeholder="나이를 입력하세요" onChange={onChangeMonth}/>
+                    <button onClick={Registration} className="reg_submit background_orange color_white bold_20">
                         완료
-                    </div>
+                    </button>
                 </form>
             </div>
         </div>
